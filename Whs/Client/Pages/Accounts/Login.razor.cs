bc58c211f4716ebfe79a40618d81e6b192b17dd6
igmo <igmo71@mail.ru>
2020-10-07 10:05:41 +0300

    Add Login page.

---
+++ Whs/Client/Pages/Accounts/Login.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Whs.Client.Identity;
+using Whs.Shared.Models.Accounts;
+
+namespace Whs.Client.Pages.Accounts
+{
+    public partial class Login
+    {
+
+        private UserForAuthenticationDto _userForAuthentication = new UserForAuthenticationDto();
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        public bool ShowAuthError { get; set; }
+        public string Error { get; set; }
+
+        private async Task ScannedBarcode(ChangeEventArgs args)
+        {
+            _userForAuthentication.Barcode = args.Value.ToString();
+            await ExecuteLogin();
+        }
+
+        public async Task ExecuteLogin()
+        {
+            ShowAuthError = false;
+            var result = await AuthenticationService.Login(_userForAuthentication);
+            if (!result.IsAuthSuccessful)
+            {
+                Error = result.ErrorMessage;
+                ShowAuthError = true;
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
