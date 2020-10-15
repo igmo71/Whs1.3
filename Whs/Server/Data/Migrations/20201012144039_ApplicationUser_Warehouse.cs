6d14294e2b7b31a20c60f75d9cff41ff588d5112
igmo <igmo@VDS-WRK-2>
2020-10-15 10:41:40 +0300

    Remake migrations for Copy database create.

--- Whs/Server/Data/Migrations/20201012144039_ApplicationUser_Warehouse.cs
+++ Whs/Server/Data/Migrations/20201012144039_ApplicationUser_Warehouse.cs
@@ -7,29 +7,29 @@ namespace Whs.Server.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
 
-            migrationBuilder.AddColumn<string>(
-                name: "WarehouseId",
-                table: "AspNetUsers",
-                nullable: true);
+            //migrationBuilder.AddColumn<string>(
+            //    name: "WarehouseId",
+            //    table: "AspNetUsers",
+            //    nullable: true);
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_AspNetUsers_Warehouses_WarehouseId",
-                table: "AspNetUsers",
-                column: "WarehouseId",
-                principalTable: "Warehouses",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.SetNull);
+            //migrationBuilder.AddForeignKey(
+            //    name: "FK_AspNetUsers_Warehouses_WarehouseId",
+            //    table: "AspNetUsers",
+            //    column: "WarehouseId",
+            //    principalTable: "Warehouses",
+            //    principalColumn: "Id",
+            //    onDelete: ReferentialAction.SetNull);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_AspNetUsers_Warehouses_WarehouseId",
-                table: "AspNetUsers");
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_AspNetUsers_Warehouses_WarehouseId",
+            //    table: "AspNetUsers");
 
-            migrationBuilder.DropColumn(
-               name: "WarehouseId",
-               table: "AspNetUsers");
+            //migrationBuilder.DropColumn(
+            //   name: "WarehouseId",
+            //   table: "AspNetUsers");
         }
     }
 }
