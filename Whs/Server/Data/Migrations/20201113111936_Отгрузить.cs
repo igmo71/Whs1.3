1d88c2c013029f37f3a2c6fd56ae8a1d867f06b2
IgMo <igmo71@mail.ru>
2020-11-13 14:40:27 +0300

    Change  Where from Оплачено to Отгрузить.

---
+++ Whs/Server/Data/Migrations/20201113111936_Отгрузить.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Whs.Server.Data.Migrations
+{
+    public partial class Отгрузить : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Отгрузить",
+                table: "WhsOrdersOut",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Отгрузить",
+                table: "WhsOrdersOut");
+        }
+    }
+}
